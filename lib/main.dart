691c1347966e567c7b532ce71c8605b6317a36ef
Sims0212 <sims0212@naver.com>
2025-05-13 15:47:44 +0900

    로그인 회원가입 페이지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,30 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
-import 'add_housing_page.dart';
-
+import 'login_page.dart'; // 로그인 페이지 import
+import 'signup_page.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/login',
+    routes: {
+      '/login': (context) => const LoginPage(),
+      '/signup': (context) => SignupPage(),
+      '/home': (context) => HomePage(),
+    },
+  ));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GUARDION',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LoginPage(), // 처음 시작할 페이지를 로그인 페이지로 설정
     );
   }
 }
