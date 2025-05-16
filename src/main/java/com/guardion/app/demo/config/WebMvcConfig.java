45d3b3bf8e0de12e114ee026f554f5ca48df69d7
kdy2224 <kdy2224@naver.com>
2025-05-16 21:34:48 +0900

    cors 설정

--- src/main/java/com/guardion/app/demo/config/WebMvcConfig.java
+++ src/main/java/com/guardion/app/demo/config/WebMvcConfig.java
@@ -1,18 +1,22 @@
 package com.guardion.app.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Value("${cors.allowed-origin}")
+	private String allowedOrigins;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins(allowedOrigins)
 			.allowedMethods("*")
 			.allowedHeaders("*")
-			.allowedOriginPatterns("*") // 개발 중일 경우만
 			.allowCredentials(true);
 	}
 }
